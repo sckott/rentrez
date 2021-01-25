16732cd1d1bd3ac2c7f73c3ad88da5eed1f5d543
Scott Chamberlain <sckott@fastmail.com>
2021-01-25 15:45:12 -0800

    update manual files

---
+++ man-roxygen/retry.R
@@ -0,0 +1,4 @@
+#' @param retry (list) use \code{\link{entrez_retry_options}} to make a named
+#' list of retry options to pass on to the HTTP request. default values are
+#' passed for you, but you can change them by setting the params in
+#' \code{\link{entrez_retry_options}} passed to this parameter
